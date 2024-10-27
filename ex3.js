b55893c2a9e025b067e7fbcf4553b37f88e9d0ed
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-10-27 18:54:21 +0200

    added ornaments .js file

--- ex3.js
+++ ex3.js
@@ -3,19 +3,33 @@ var x = parseInt(prompt("Enter x:"));
 function generateTree(x) {
     var Tree = "";
 
+    var firstRow = " ";
+    for (var s = 1; s < x; s++) {
+        firstRow += " ";
+    }
+    firstRow += "*\n";
+    Tree += firstRow;
+
     for (var i = 1; i <= x; i++) {
         var row = "";
         for (var j = 1; j <= x - i; j++) {
             row += " ";
         }
 
+
         for (var k = 1; k <= 2 * i - 1; k++) {
-            if (k == 1 || k == 2 * i - 1) {
+            if (k == 1) {
                 row += "*";
-            } else {
+            }
+            if (Math.random() < 0.09) {
+                const ornaments = ["o", "#"];
+                const randomOrnament = ornaments[Math.floor(Math.random() * ornaments.length)];
+                row += randomOrnament;
+            }else {
                 row += "|";
             }
         }
+            row += "*";
 
         Tree += row + "\n";
     }
