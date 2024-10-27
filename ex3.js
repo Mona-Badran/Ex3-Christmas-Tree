804071639f437d4ffb626a60ada5f2e0c0cfa07a
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-10-27 19:33:55 +0200

    added stem

--- ex3.js
+++ ex3.js
@@ -16,25 +16,42 @@ function generateTree(x) {
             row += " ";
         }
 
-
         for (var k = 1; k <= 2 * i - 1; k++) {
             if (k == 1) {
                 row += "*";
             }
             if (Math.random() < 0.09) {
                 const ornaments = ["o", "#"];
-                const randomOrnament = ornaments[Math.floor(Math.random() * ornaments.length)];
+                const randomOrnament =
+                    ornaments[Math.floor(Math.random() * ornaments.length)];
                 row += randomOrnament;
-            }else {
+            } else {
                 row += "|";
             }
         }
-            row += "*";
+        row += "*";
 
         Tree += row + "\n";
     }
+    var stemWidth = Math.floor(x / 3);
+    var stemHeight = Math.floor(x / 3);
+    var stemPadding = Math.floor((2 * x - 1 - stemWidth) / 2);
 
-    preElement = document.createElement("pre"); 
+    for (var i = 0; i < stemHeight; i++) {
+        var stemRow = "";
+
+        for (var j = 0; j < stemPadding; j++) {
+            stemRow += " ";
+        }
+
+        for (var k = 0; k < stemWidth; k++) {
+            stemRow += "|";
+        }
+
+        Tree += stemRow + "\n";
+    }
+
+    var preElement = document.createElement("pre");
     preElement.innerText = Tree;
 
     document.body.appendChild(preElement);
